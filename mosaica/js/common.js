e3eda93a3aed03daff4f5fff18ac89a55ec677be
Ilya <pw.i.like@mail.ru>
2018-12-14 18:01:01 +0300

    mosaica

---
+++ mosaica/js/common.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function() {
+
+	var $container = $(".masonry-container");
+	$container.imagesLoaded(function () {
+		$container.masonry({
+			columnWidth: ".item",
+			itemSelector: ".item"
+		});
+		$(".item").imagefill();
+	});
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	});
