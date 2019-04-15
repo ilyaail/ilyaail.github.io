4bbfd065ffb413f123c77b67d461849e1eecd87e
Ilya <pw.i.like@mail.ru>
2019-04-15 16:05:29 +0300

    createdTESTING

---
+++ ligabiz/js/main.js
@@ -0,0 +1,15 @@
+/**/
+jQuery(document).ready(function($){	
+
+
+		$(".js-video-button").modalVideo({
+			youtube:{
+				controls:0,
+				nocookie: true
+			}
+		});
+$(".c_back").fancybox({ href: '#form_call_back', wrapCSS: 'modal_call_back'});
+
+});
+/**/
+
