b7c4b9c33edae3b540651129858c0f50236347dc
Ilya <pw.i.like@mail.ru>
2019-04-16 23:42:38 +0300

    created site

---
+++ landing/js/common.js
@@ -0,0 +1,37 @@
+$(function() {
+
+	// Custom JS
+$('.testimonials__slider').slick({
+	slidesToShow: 1,
+	loop:true,
+	dots: true,
+	arrows:false,
+	responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+     }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+         arrows: false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+         arrows: false,
+      }
+    }
+    ]
+});
+});
