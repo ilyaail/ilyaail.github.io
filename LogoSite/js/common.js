abb108b222f6928944a14311235762eef956bbe3
Ilya <pw.i.like@mail.ru>
2019-04-17 20:18:22 +0300

    edited

--- LogoSite/js/common.js
+++ LogoSite/js/common.js
@@ -1,25 +1,41 @@
-$( document ).ready(function() {
+$(function() {
 
-		$(".js-video-button").modalVideo({
-			youtube:{
-				controls:0,
-				nocookie: true
-			}
-		});
-			var scroll = new SmoothScroll('a[href*="#"]',{
-				speed: 1000,
-				speedAsDuration: true
-			});
-
-  $('.portfolio-slider').slick({
-    dots: true,
-    speed: 350,
-    infiniti: true,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    responsive: true
-  });
-  jQuery(function($){
-   $("#phone_input").mask("+12 (399) 9999");
+	// Custom JS
+$('.testimonials__slider').slick({
+	slidesToShow: 1,
+	loop:true,
+	dots: true,
+	arrows:false,
+	responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+     }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+         arrows: false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+         arrows: false,
+      }
+    }
+    ]
+});
+jQuery(function($){
+   $("#phone_input").mask("+79 (999) 9999");
 });
 });
+
