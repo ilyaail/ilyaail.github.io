953771b375a98d40ad24c21234bd7638fc7bf705
ilyaail <40268706+ilyaail@users.noreply.github.com>
2019-06-21 01:37:56 +0300

    Add files via upload

---
+++ new-dent/js/common.js
@@ -0,0 +1,33 @@
+$(function() {
+
+	// Custom JS
+$('.hi_slider').slick({
+	dots:true,
+	infinite: true,
+	arrows: false
+});
+
+
+});
+$(document).ready(function(){
+(function($) {
+$(function() {
+ 
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+ 
+});
+})(jQuery);
+  var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
+});
